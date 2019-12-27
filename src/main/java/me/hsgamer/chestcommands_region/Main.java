c7fc6c4015a05bcb5b7c71bb0fdcb7873aca5351
HSGamer <huynhqtienvtag@gmail.com>
2019-12-27 18:00:45 +0700

    optimize

--- src/main/java/me/hsgamer/chestcommands_region/Main.java
+++ src/main/java/me/hsgamer/chestcommands_region/Main.java
@@ -2,7 +2,6 @@ package me.hsgamer.chestcommands_region;
 
 import com.gmail.filoghost.chestcommands.api.Addon;
 import com.gmail.filoghost.chestcommands.serializer.RequirementSerializer;
-import org.bukkit.Bukkit;
 
 public class Main extends Addon {
 
@@ -13,7 +12,7 @@ public class Main extends Addon {
   @Override
   public void onEnable() {
     RequirementSerializer.register("WORLD", WorldRequirement.class);
-    if (Bukkit.getPluginManager().isPluginEnabled("WorldGuard")) {
+    if (getPlugin().getServer().getPluginManager().isPluginEnabled("WorldGuard")) {
       RequirementSerializer.register("REGION", RegionRequirement.class);
     }
   }
--- src/main/java/me/hsgamer/chestcommands_region/RegionRequirement.java
+++ src/main/java/me/hsgamer/chestcommands_region/RegionRequirement.java
@@ -2,7 +2,6 @@ package me.hsgamer.chestcommands_region;
 
 import com.gmail.filoghost.chestcommands.api.IconRequirement;
 import java.util.List;
-import java.util.Set;
 import org.bukkit.entity.Player;
 import org.codemc.worldguardwrapper.WorldGuardWrapper;
 import org.codemc.worldguardwrapper.region.IWrappedRegion;
@@ -16,8 +15,7 @@ public class RegionRequirement extends IconRequirement {
   @Override
   public boolean check(Player player) {
     List<Object> values = getParsedValue(player);
-    Set<IWrappedRegion> regions = WorldGuardWrapper.getInstance().getRegions(player.getLocation());
-    for (IWrappedRegion region : regions) {
+    for (IWrappedRegion region : WorldGuardWrapper.getInstance().getRegions(player.getLocation())) {
       if (values.contains(region.getId())) {
         return true;
       }
